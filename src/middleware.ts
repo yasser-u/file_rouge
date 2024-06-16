f1026a8aee06212a7056918d9409639b57514e57
yasser-u <yasser2959@gmail.com>
2024-06-16 20:51:41 +0200

    correction for deployment

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,18 +3,18 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
     // Exclure les routes dans (preConnection)
-    if (request.nextUrl.pathname.startsWith('/(preConnection)')) {
-        return NextResponse.next();
-    }
+    // if (request.nextUrl.pathname.startsWith('/(preConnection)')) {
+    //     return NextResponse.next();
+    // }
 
     // Logique du middleware
     // Vérifie la présence du cookie de session
-    const sessionCookie = request.cookies.get('appwriteSession')
-
-    if (!sessionCookie) {
-        // Si pas de session, redirige vers le login
-        return NextResponse.redirect(new URL('/', request.url))
-    }
+    // const sessionCookie = request.cookies.get('appwriteSession')
+    //
+    // if (!sessionCookie) {
+    //     // Si pas de session, redirige vers le login
+    //     return NextResponse.redirect(new URL('/', request.url))
+    // }
 
     // Si session valide, passe la requête à la route
     return NextResponse.next()
