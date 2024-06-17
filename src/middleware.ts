9e8e35e0427807e0eb448e29666ecb9e644b4266
yasser-u <yasser2959@gmail.com>
2024-06-17 12:18:09 +0200

    correction for deployment try with middleware: abandon

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,19 +2,19 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-    const isAuthenticated = request.cookies.has('appwriteSession');
-    const isAuthPage =
-        request.nextUrl.pathname === '/sign-in' ||
-        request.nextUrl.pathname === '/sign-up';
-    const isHomePage = request.nextUrl.pathname === '/';
-
-    if (isAuthenticated && isAuthPage) {
-        return NextResponse.redirect(new URL('/accueil', request.url));
-    }
-
-    if (!isAuthenticated && !isAuthPage && !isHomePage) {
-        return NextResponse.redirect(new URL('/sign-in', request.url));
-    }
+    // const isAuthenticated = request.cookies.has('appwriteSession');
+    // const isAuthPage =
+    //     request.nextUrl.pathname === '/sign-in' ||
+    //     request.nextUrl.pathname === '/sign-up';
+    // const isHomePage = request.nextUrl.pathname === '/';
+    //
+    // if (isAuthenticated && isAuthPage) {
+    //     return NextResponse.redirect(new URL('/accueil', request.url));
+    // }
+    //
+    // if (!isAuthenticated && !isAuthPage && !isHomePage) {
+    //     return NextResponse.redirect(new URL('/sign-in', request.url));
+    // }
 
     return NextResponse.next();
 }
